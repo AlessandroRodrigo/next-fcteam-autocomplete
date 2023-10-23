d5a7328f4efa4a95f0b84f15195cfb5f724cd8ed
jvitormelo-t <jvitor.ovidio2@hotmail.com>
2023-10-23 16:43:57 -0300

    feat: add basic pwa manifesto and service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+// Service Worker Script
+
+const CACHE_NAME = "cache-v1";
+
+const urlsToCache = ["/", "/home", "/public/512.png", "/public/favicon.ico"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
